7ca67d45ea3236f236121b34c0a1f72b76ad56b4
kabishbrt <kabishbrt@gmail.com>
2024-07-21 16:22:54 +0545

    .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./css/style.css";
-import RoutesEntryPoint from "./routes";
+import RoutesEntryPoint from "./App";
 
 ReactDOM.render(<RoutesEntryPoint />, document.getElementById("root"));
